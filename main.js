9432f663bb7a965559ea290f6fe4d5d5b5725f6d
Luke Park <143543147+lukep258@users.noreply.github.com>
2023-10-24 15:40:34 -0500

    around and round

--- main.js
+++ main.js
@@ -5,19 +5,31 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server, {
-    connectionStateRecovery: {}
-  });
+const io = new Server(server);
+const joinedIDs = {};
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
+  joinedIDs[socket.id]=Object.keys(joinedIDs).length;
+  console.log(socket.id)
+  console.log(joinedIDs[socket.id])
+  socket.join(joinedIDs[socket.id])
+  if(joinedIDs[socket.id]===0){
+    io.to(joinedIDs[socket.id]).emit('chat message','start the game')
     socket.on('chat message', (msg) => {
         console.log('server received')
-        io.emit('chat message', msg);
+        console.log(joinedIDs[socket.id]+1)
+        io.to(joinedIDs[socket.id]+1).emit('chat message', msg);
     });
+  }
+  else{}
+  io.on('disconnection',(socket)=>{
+    socket.leave(joinedIDs[socketID])
+    delete joinedIDs[socketID]
+  })
 });
 
 server.listen(3000, () => {
